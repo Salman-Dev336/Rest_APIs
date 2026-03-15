34a8bb63bb0076c95be2e7ea9546b1d21bd61a69
Salman Khan <salmantechhub336@gmail.com>
2026-03-16 03:51:33 +0500

    create most complex model

--- lib/model_name.dart
+++ lib/model_name.dart
@@ -1,123 +1,276 @@
-// ignore_for_file: prefer_collection_literals, unnecessary_this, unnecessary_new, camel_case_types
+// ignore_for_file: unnecessary_new, prefer_collection_literals, unnecessary_this
 
 class modelName {
-  int? id;
-  String? name;
-  String? username;
-  String? email;
-  Address? address;
-  String? phone;
-  String? website;
-  Company? company;
+  bool? success;
+  String? message;
+  List<Data>? data;
 
-  modelName(
-      {this.id,
-      this.name,
-      this.username,
-      this.email,
-      this.address,
-      this.phone,
-      this.website,
-      this.company});
+  modelName({this.success, this.message, this.data});
 
   modelName.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
+    success = json['success'];
+    message = json['message'];
+    if (json['data'] != null) {
+      data = <Data>[];
+      json['data'].forEach((v) {
+        data!.add(new Data.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['success'] = this.success;
+    data['message'] = this.message;
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Data {
+  String? sId;
+  bool? onSale;
+  int? salePercent;
+  int? sold;
+  bool? sliderNew;
+  bool? sliderRecent;
+  bool? sliderSold;
+  String? date;
+  String? title;
+  Categories? categories;
+  Subcat? subcat;
+  Shop? shop;
+  String? price;
+  String? saleTitle;
+  String? salePrice;
+  String? description;
+  String? color;
+  String? size;
+  bool? inWishlist;
+  List<Images>? images;
+
+  Data(
+      {this.sId,
+      this.onSale,
+      this.salePercent,
+      this.sold,
+      this.sliderNew,
+      this.sliderRecent,
+      this.sliderSold,
+      this.date,
+      this.title,
+      this.categories,
+      this.subcat,
+      this.shop,
+      this.price,
+      this.saleTitle,
+      this.salePrice,
+      this.description,
+      this.color,
+      this.size,
+      this.inWishlist,
+      this.images});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    onSale = json['on_sale'];
+    salePercent = json['sale_percent'];
+    sold = json['sold'];
+    sliderNew = json['slider_new'];
+    sliderRecent = json['slider_recent'];
+    sliderSold = json['slider_sold'];
+    date = json['date'];
+    title = json['title'];
+    categories = json['categories'] != null
+        ? new Categories.fromJson(json['categories'])
+        : null;
+    subcat =
+        json['subcat'] != null ? new Subcat.fromJson(json['subcat']) : null;
+    shop = json['shop'] != null ? new Shop.fromJson(json['shop']) : null;
+    price = json['price'];
+    saleTitle = json['sale_title'];
+    salePrice = json['sale_price'];
+    description = json['description'];
+    color = json['color'];
+    size = json['size'];
+    inWishlist = json['in_wishlist'];
+    if (json['images'] != null) {
+      images = <Images>[];
+      json['images'].forEach((v) {
+        images!.add(new Images.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['on_sale'] = this.onSale;
+    data['sale_percent'] = this.salePercent;
+    data['sold'] = this.sold;
+    data['slider_new'] = this.sliderNew;
+    data['slider_recent'] = this.sliderRecent;
+    data['slider_sold'] = this.sliderSold;
+    data['date'] = this.date;
+    data['title'] = this.title;
+    if (this.categories != null) {
+      data['categories'] = this.categories!.toJson();
+    }
+    if (this.subcat != null) {
+      data['subcat'] = this.subcat!.toJson();
+    }
+    if (this.shop != null) {
+      data['shop'] = this.shop!.toJson();
+    }
+    data['price'] = this.price;
+    data['sale_title'] = this.saleTitle;
+    data['sale_price'] = this.salePrice;
+    data['description'] = this.description;
+    data['color'] = this.color;
+    data['size'] = this.size;
+    data['in_wishlist'] = this.inWishlist;
+    if (this.images != null) {
+      data['images'] = this.images!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Categories {
+  String? sId;
+  String? type;
+  int? salePercent;
+  String? date;
+  String? name;
+  String? image;
+
+  Categories(
+      {this.sId,
+      this.type,
+      this.salePercent,
+      this.date,
+      this.name,
+      this.image});
+
+  Categories.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    type = json['type'];
+    salePercent = json['sale_percent'];
+    date = json['date'];
     name = json['name'];
-    username = json['username'];
-    email = json['email'];
-    address =
-        json['address'] != null ? new Address.fromJson(json['address']) : null;
-    phone = json['phone'];
-    website = json['website'];
-    company =
-        json['company'] != null ? new Company.fromJson(json['company']) : null;
+    image = json['image'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['type'] = this.type;
+    data['sale_percent'] = this.salePercent;
+    data['date'] = this.date;
+    data['name'] = this.name;
+    data['image'] = this.image;
+    return data;
+  }
+}
+
+class Subcat {
+  String? sId;
+  String? type;
+  int? salePercent;
+  String? date;
+  String? name;
+
+  Subcat({this.sId, this.type, this.salePercent, this.date, this.name});
+
+  Subcat.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    type = json['type'];
+    salePercent = json['sale_percent'];
+    date = json['date'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['type'] = this.type;
+    data['sale_percent'] = this.salePercent;
+    data['date'] = this.date;
+    data['name'] = this.name;
+    return data;
+  }
+}
+
+class Shop {
+  String? sId;
+  bool? isActive;
+  String? createdAt;
+  String? name;
+  String? description;
+  String? shopemail;
+  String? shopaddress;
+  String? shopcity;
+  String? userid;
+  String? image;
+
+  Shop(
+      {this.sId,
+      this.isActive,
+      this.createdAt,
+      this.name,
+      this.description,
+      this.shopemail,
+      this.shopaddress,
+      this.shopcity,
+      this.userid,
+      this.image});
+
+  Shop.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    isActive = json['is_active'];
+    createdAt = json['created_At'];
+    name = json['name'];
+    description = json['description'];
+    shopemail = json['shopemail'];
+    shopaddress = json['shopaddress'];
+    shopcity = json['shopcity'];
+    userid = json['userid'];
+    image = json['image'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['is_active'] = this.isActive;
+    data['created_At'] = this.createdAt;
+    data['name'] = this.name;
+    data['description'] = this.description;
+    data['shopemail'] = this.shopemail;
+    data['shopaddress'] = this.shopaddress;
+    data['shopcity'] = this.shopcity;
+    data['userid'] = this.userid;
+    data['image'] = this.image;
+    return data;
+  }
+}
+
+class Images {
+  String? id;
+  String? url;
+
+  Images({this.id, this.url});
+
+  Images.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    url = json['url'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['id'] = this.id;
-    data['name'] = this.name;
-    data['username'] = this.username;
-    data['email'] = this.email;
-    if (this.address != null) {
-      data['address'] = this.address!.toJson();
-    }
-    data['phone'] = this.phone;
-    data['website'] = this.website;
-    if (this.company != null) {
-      data['company'] = this.company!.toJson();
-    }
-    return data;
-  }
-}
-
-class Address {
-  String? street;
-  String? suite;
-  String? city;
-  String? zipcode;
-  Geo? geo;
-
-  Address({this.street, this.suite, this.city, this.zipcode, this.geo});
-
-  Address.fromJson(Map<String, dynamic> json) {
-    street = json['street'];
-    suite = json['suite'];
-    city = json['city'];
-    zipcode = json['zipcode'];
-    geo = json['geo'] != null ? new Geo.fromJson(json['geo']) : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['street'] = this.street;
-    data['suite'] = this.suite;
-    data['city'] = this.city;
-    data['zipcode'] = this.zipcode;
-    if (this.geo != null) {
-      data['geo'] = this.geo!.toJson();
-    }
-    return data;
-  }
-}
-
-class Geo {
-  String? lat;
-  String? lng;
-
-  Geo({this.lat, this.lng});
-
-  Geo.fromJson(Map<String, dynamic> json) {
-    lat = json['lat'];
-    lng = json['lng'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['lat'] = this.lat;
-    data['lng'] = this.lng;
-    return data;
-  }
-}
-
-class Company {
-  String? name;
-  String? catchPhrase;
-  String? bs;
-
-  Company({this.name, this.catchPhrase, this.bs});
-
-  Company.fromJson(Map<String, dynamic> json) {
-    name = json['name'];
-    catchPhrase = json['catchPhrase'];
-    bs = json['bs'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['name'] = this.name;
-    data['catchPhrase'] = this.catchPhrase;
-    data['bs'] = this.bs;
+    data['url'] = this.url;
     return data;
   }
 }
