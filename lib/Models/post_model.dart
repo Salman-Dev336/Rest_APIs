2c9ef66d48c9e57763bcf91050253661d95ae17c
Salman Khan <salmantechhub336@gmail.com>
2026-03-16 04:13:31 +0500

    create model by using jsontodart extension

---
+++ lib/Models/post_model.dart
@@ -0,0 +1,29 @@
+
+class PostModel {
+  int? userId;
+  int? id;
+  String? title;
+  String? body;
+
+  PostModel({this.userId, this.id, this.title, this.body});
+
+  PostModel.fromJson(Map<String, dynamic> json) {
+    userId = json["userId"];
+    id = json["id"];
+    title = json["title"];
+    body = json["body"];
+  }
+
+  static List<PostModel> fromList(List<Map<String, dynamic>> list) {
+    return list.map(PostModel.fromJson).toList();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> _data = <String, dynamic>{};
+    _data["userId"] = userId;
+    _data["id"] = id;
+    _data["title"] = title;
+    _data["body"] = body;
+    return _data;
+  }
+}
