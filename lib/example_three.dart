0d755c62a45d6177c6746f2bdc1c6c4eed8934fe
Salman Khan <salmantechhub336@gmail.com>
2026-03-19 16:43:43 +0500

    hit api

--- lib/example_three.dart
+++ lib/example_three.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:rest_api/Models/user_model.dart';
@@ -14,10 +16,13 @@ class _ExampleThreeState extends State<ExampleThree> {
   Future<List<UserModel>> getUserApi() async {
     final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
 
-    var data = response.body.toString();
+    var data = jsonDecode(response.body.toString());
 
     if (response.statusCode == 200){
-      userList = UserModel().fromList(data);
+      for(Map<String, dynamic> i in data){
+        userList.add( UserModel.fromJson(i));
+      }
+      // userList = UserModel().fromList(data);
       return userList;
     }
     else{
