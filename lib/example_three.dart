000eb201c18019809b427c6d01a0a6e6f52cd839
Salman Khan <salmantechhub336@gmail.com>
2026-03-19 16:41:19 +0500

    calling an api

--- lib/example_three.dart
+++ lib/example_three.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:rest_api/Models/user_model.dart';
 
 class ExampleThree extends StatefulWidget {
   const ExampleThree({super.key});
@@ -9,16 +10,28 @@ class ExampleThree extends StatefulWidget {
 }
 
 class _ExampleThreeState extends State<ExampleThree> {
+  List<UserModel> userList = [];
+  Future<List<UserModel>> getUserApi() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+    var data = response.body.toString();
+
+    if (response.statusCode == 200){
+      userList = UserModel().fromList(data);
+      return userList;
+    }
+    else{
+      return userList;
+    }
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('API Course'),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          
+      appBar: AppBar(title: Text('API Course'), centerTitle: true),
+      body: Column(children: [
+
         ],
       ),
     );
