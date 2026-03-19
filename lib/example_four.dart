f0b0459262e2d533b850a730ef91aca9cd0e6f3c
Salman Khan <salmantechhub336@gmail.com>
2026-03-20 03:39:06 +0500

    create reusable componenet

--- lib/example_four.dart
+++ lib/example_four.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_typing_uninitialized_variables, unused_local_variable
+// ignore_for_file: must_be_immutable, prefer_typing_uninitialized_variables, unused_local_variable
 
 import 'dart:convert';
 
@@ -59,13 +59,43 @@ class _ExampleFourState extends State<ExampleFour> {
                }
 
                else{
-                return Text(data[0]['name'].toString());
+                return ListView.builder(
+                  itemBuilder: (context, index){
+                    return Card(
+                      child: Column(
+                        children: [
+
+                        ],
+                      ),
+
+                    );
+
+                  }
+                  );
                 
                }
               
               },
               ),
             ),
+        ],
+      ),
+    );
+  }
+}
+class ReusableRow  extends StatelessWidget{
+  String title, value;
+  ReusableRow({super.key, required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title),
+          Text(value),
         ],
       ),
     );
