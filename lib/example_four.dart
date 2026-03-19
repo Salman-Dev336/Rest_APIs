954b1b69a0ca3213509fee5544810b3f5343a822
Salman Khan <salmantechhub336@gmail.com>
2026-03-20 03:18:45 +0500

    updation

--- lib/example_four.dart
+++ lib/example_four.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:meta/meta_meta.dart';
 import 'package:http/http.dart' as http;
@@ -11,6 +13,18 @@ class ExampleFour extends StatefulWidget {
 }
 
 class _ExampleFourState extends State<ExampleFour> {
+
+  Future<void>  getUserApi() async{
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200){
+      for(Map<String, dynamic> i in data){
+        userList.add (UserModel.fromJson( i));  
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
