05956d0578f017c43476d7046a7fd10924b48edb
Salman Khan <salmantechhub336@gmail.com>
2026-03-20 03:10:29 +0500

    calling example four in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rest_api/exampleTwo.dart';
+import 'package:rest_api/example_four.dart';
 import 'package:rest_api/example_three.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: ExampleThree(),
+      home: ExampleFour(),
     );
   }
 }
