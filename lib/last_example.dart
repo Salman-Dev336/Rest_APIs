6697c3590f95d9ad6c692a4f6cd1a370d358d9d4
Salman Khan <salmantechhub336@gmail.com>
2026-03-22 08:06:42 +0500

    last example on calling an api

---
+++ lib/last_example.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:rest_api/Models/user_model.dart';
+
+class LastExample extends StatefulWidget {
+  const LastExample({super.key});
+
+  @override
+  State<LastExample> createState() => _LastExampleState();
+}
+
+class _LastExampleState extends State<LastExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
