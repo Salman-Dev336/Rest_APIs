a72de9a26339a2f3e14d0e9cf4a6694ab1ac3e8f
Salman Khan <salmantechhub336@gmail.com>
2026-03-22 08:14:06 +0500

    updation

--- lib/last_example.dart
+++ lib/last_example.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:rest_api/Models/products_model.dart';
 import 'dart:convert';
 import 'package:rest_api/Models/user_model.dart';
 
@@ -11,6 +12,11 @@ class LastExample extends StatefulWidget {
 }
 
 class _LastExampleState extends State<LastExample> {
+Future<ProductsModel> getProductsAPI() async {
+  final response = await http.get(Uri.parse('https://webhook.site/8e8858b7-c47f-4b68-817f-fce4acfcb4c5'));
+
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
