de9b1e79917a05c1816ec7ed4b5c6acd82bf77f6
Salman Khan <salmantechhub336@gmail.com>
2026-04-01 22:22:29 +0500

    changes

---
+++ lib/upload_image.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class UploadImageScreen extends StatefulWidget {
+  const UploadImageScreen({super.key});
+
+  @override
+  State<UploadImageScreen> createState() => _UploadImageScreenState();
+}
+
+class _UploadImageScreenState extends State<UploadImageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Upload Image'),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+      ),
+    );
+  }
+}
